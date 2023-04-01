9239474437b04d75f85902c9fe1ed5c68627681d
Ovidiu Rosoiu <REDACTED>
2023-04-01 19:40:33 +0300

    Add Firefox extension code

---
+++ firefox-extension/config/options.js
@@ -0,0 +1,22 @@
+function saveOptions(e) {
+	browser.storage.sync.set({
+		squeezifyOptions: {
+			playerID: document.querySelector("#playerID").value,
+			lmsHost: document.querySelector("#lmsHost").value,
+			lmsPort: document.querySelector("#lmsPort").value
+		}
+	});
+	e.preventDefault();
+}
+
+function restoreOptions() {
+  let squeezifyOptionsGet = browser.storage.sync.get('squeezifyOptions');
+  squeezifyOptionsGet.then((res) => {
+    document.querySelector("#playerID").value = res.squeezifyOptions.playerID;
+    document.querySelector("#lmsHost").value = res.squeezifyOptions.lmsHost;
+    document.querySelector("#lmsPort").value = res.squeezifyOptions.lmsPort;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
